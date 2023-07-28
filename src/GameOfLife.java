32ff9d5db953aa4c6493b18b475a084dfbef11a6
Blue <didierdelvalle17@gmail.com>
2023-07-28 03:51:22 -0700

    Implementación de funciones para llenado de tablero siguiendo un patrón recibido por linea de comandos e impresión repetitiva del tablero hasta conseguir un numero iteraciones

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -6,8 +6,8 @@ public class GameOfLife {
         int HEIGHT = 0;
         int SPEED = 0;
         int GENERATIONS = 0;
-        String pattern = "RND";
         boolean run = false;
+        String pattern = "RND";
 
         for (String arg:args){
             String[] keyValue = arg.split("=");
@@ -29,31 +29,72 @@ public class GameOfLife {
             }
         }
         if (validateValues(WIDTH, HEIGHT, SPEED, GENERATIONS, pattern)) run = true;
-        //if (validateValues(WIDTH, HEIGHT, SPEED, GENERATIONS, pattern))System.out.println(pattern);
         boolean[][] board = createBoard(WIDTH, HEIGHT);
-        if (pattern.equals("RND")){
-            fillRandomly(board);
+        fillBoard(board, pattern);
+        runGame(board, run, GENERATIONS, SPEED);
+    }
+
+    private static void runGame(boolean[][] board, boolean run, int GENERATIONS, int SPEED){
+        int gen = 1;
+        if(GENERATIONS >= 0){
+            run = true;
         }
-        printBoard(board);
+        while (run) {
+            System.out.println("Generación: " + gen);
+            printBoard(board);
+            gen++;
+
+            try {
+                Thread.sleep(SPEED);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            if(GENERATIONS > 0 && gen > GENERATIONS){
+                run = false;
+            }
+        }
     }
 
     private static boolean[][] createBoard(int width, int height) {
         return new boolean[height][width];
     }
 
-    private static void fillRandomly(boolean[][] board){
+    private static void fillBoard(boolean[][] board, String pattern){
         Random random = new Random();
-        for (int row = 0; row < board.length; row++){
-            for (int col = 0; col < board[0].length; col++){
-                board[row][col] = random.nextBoolean();
+        if (pattern.equals("RND")){
+            for (int row = 0; row < board.length; row++) {
+                for (int col = 0; col < board[0].length; col++) {
+                    board[row][col] = random.nextBoolean();
+                }
+            }
+        } else {
+            String[] rows = pattern.split("#");
+            int numRows = rows.length;
+            int numCols = rows[0].length();
+
+            if (numRows > board.length || numCols > board[0].length){
+                System.out.println("El patrón ingresado no cumple con los requerimientos de tamaño");
+                return;
+            }
+
+            for (int row = 0; row < numRows; row++){
+                for (int col = 0; col < numCols; col++){
+                    char cellChar = rows[row].charAt(col);
+                    if (cellChar == '1') board[row][col] = true;
+                    else if (cellChar == '0') board[row][col] = false;
+                    else {
+                        System.out.println("El patrón contiene valores invalidos");
+                        return;
+                    }
+                }
             }
         }
     }
 
     public static void printBoard(boolean[][] board){
-        for (int row = 0; row < board.length; row++){
-            for (int col = 0; col < board[0].length; col++){
-                char cellChar = board[row][col] ? '0' : 'X';
+        for (boolean[] booleans : board) {
+            for (int col = 0; col < board[0].length; col++) {
+                char cellChar = booleans[col] ? '0' : 'X';
                 System.out.print(cellChar + " ");
             }
             System.out.println();
